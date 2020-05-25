a407619a2dca082ba4fec589e0154fd32ac55c0b
teekoksiang <teekoksiang89@gmail.com>
2020-05-25 12:30:49 +0800

    Add AppToolbar and BottomNavbar component

--- src/App.js
+++ src/App.js
@@ -1,42 +1,31 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import { HomePage } from './pages/home/HomePage';
 import { StudyGroupPage } from './pages/study-group/StudyGroupPage';
 import { VirtualGroupPage } from './pages/virtual-group/VirtualGroupPage';
 import { QAThreadPage } from './pages/qa-thread/QAThreadPage';
+import { AppToolbar } from './components/AppToolbar/AppToolbar';
+import { BottomNavbar } from './components/BottomNavbar/BottomNavbar';
 import './App.css';
 
 const App = () => {
   return (
     <Router>
       <div className="App">
-        <HomePage></HomePage>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/study-group">Study Group</Link>
-            </li>
-            <li>
-              <Link to="/virtual-group">Virtual Group</Link>
-            </li>
-            <li>
-              <Link to="/qa-thread">Q & A Thread</Link>
-            </li>
-          </ul>
-        </nav>
+        <AppToolbar />
+        <Switch>
+          <Route exact path="/">
+            <StudyGroupPage />
+          </Route>
+          <Route path="/virtual-group">
+            <VirtualGroupPage />
+          </Route>
+          <Route path="/qa-thread">
+            <QAThreadPage />
+          </Route>
+        </Switch>
+        <BottomNavbar />
       </div>
-      <Switch>
-        <Route path="/study-group">
-          <StudyGroupPage />
-        </Route>
-        <Route path="/virtual-group">
-          <VirtualGroupPage />
-        </Route>
-        <Route path="/qa-thread">
-          <QAThreadPage />
-        </Route>
-      </Switch>
     </Router>
   );
 };
---
+++ src/components/BottomNavbar/BottomNavbar.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { NavLink, withRouter } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import LocalLibraryIcon from '@material-ui/icons/LocalLibrary';
+import ContactMailIcon from '@material-ui/icons/ContactMail';
+import QuestionAnswerIcon from '@material-ui/icons/QuestionAnswer';
+
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+    position: 'fixed',
+    bottom: 0,
+  },
+});
+
+const BottomNav = ({ location }) => {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(location.pathname);
+
+  return (
+    <BottomNavigation
+      value={value}
+      onChange={(event, newValue) => {
+        console.log(location.pathname);
+        setValue(newValue);
+      }}
+      showLabels
+      className={classes.root}
+    >
+      <BottomNavigationAction
+        component={NavLink}
+        to="/"
+        value="/"
+        label="Study Group"
+        icon={<LocalLibraryIcon />}
+      />
+      <BottomNavigationAction
+        component={NavLink}
+        to="/virtual-group"
+        value="/virtual-group"
+        label="Virtual Group"
+        icon={<ContactMailIcon />}
+      />
+      <BottomNavigationAction
+        component={NavLink}
+        to="/qa-thread"
+        value="/qa-thread"
+        label="Q & A Thread"
+        icon={<QuestionAnswerIcon />}
+      />
+    </BottomNavigation>
+  );
+};
+
+export const BottomNavbar = withRouter(BottomNav);
