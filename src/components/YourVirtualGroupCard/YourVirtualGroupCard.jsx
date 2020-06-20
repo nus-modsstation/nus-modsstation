7cce07f4f07c2359fa10860a184dada80f0c0ca6
Tee Kok Siang <teekoksiang89@gmail.com>
2020-06-20 23:11:37 +0800

    Clear warning

--- src/components/YourVirtualGroupCard/YourVirtualGroupCard.jsx
+++ src/components/YourVirtualGroupCard/YourVirtualGroupCard.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { materialStyles } from '../../styles/material.styles';
 import { Typography } from '@material-ui/core';
 import { Button } from '@material-ui/core';
 import { ListItem } from '@material-ui/core';
