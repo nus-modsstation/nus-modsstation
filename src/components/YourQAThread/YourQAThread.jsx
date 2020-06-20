b579ffa4bc7cd48853e1609311ef5d243bade6c3
Tee Kok Siang <teekoksiang89@gmail.com>
2020-06-20 23:29:19 +0800

    Clear warnings

--- src/components/YourQAThread/YourQAThread.jsx
+++ src/components/YourQAThread/YourQAThread.jsx
@@ -1,20 +1,10 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import { materialStyles } from "../../styles/material.styles";
 import { Typography } from "@material-ui/core";
 import { Button } from "@material-ui/core";
 import { Box } from "@material-ui/core";
 import { Paper } from "@material-ui/core";
 import { Grid } from "@material-ui/core";
-
-const componentStyles = makeStyles({
-  item: {
-    display: "flex",
-    flexDirection: "column",
-    background: "white",
-    borderBottom: "0.5px solid rgba(0, 0, 0, 0.35)",
-  },
-});
 
 export const YourQAThread = () => {
   const materialClasses = materialStyles();
