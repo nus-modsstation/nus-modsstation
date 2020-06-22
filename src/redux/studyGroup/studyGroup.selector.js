d6021e5adb63b387eadf29862059943ed8aec485
teekoksiang <teekoksiang89@gmail.com>
2020-06-22 22:05:30 +0800

    Filter out past study groups with selector

--- src/redux/studyGroup/studyGroup.selector.js
+++ src/redux/studyGroup/studyGroup.selector.js
@@ -1,14 +1,26 @@
+import moment from 'moment';
 import { createSelector } from 'reselect';
 import { StudyGroup } from '../../models/StudyGroup';
 
 const selectStudyGroup = (state) => state.studyGroup;
 
-export const selectMyGroups = createSelector([selectStudyGroup], (studyGroup) =>
-  studyGroup.myGroups.map((group) => StudyGroup.fromJson(group))
+export const selectMyGroups = createSelector(
+  [selectStudyGroup],
+  (studyGroup) => {
+    const now = moment();
+    return studyGroup.myGroups
+      .map((group) => StudyGroup.fromJson(group))
+      .filter((group) => group.startTime.isAfter(now));
+  }
 );
 
 export const selectStudyGroupsByModule = (moduleCode) =>
-  createSelector([selectStudyGroup], (studyGroup) => studyGroup[moduleCode]);
+  createSelector([selectStudyGroup], (studyGroup) => {
+    const now = moment();
+    return studyGroup[moduleCode]
+      .map((group) => StudyGroup.fromJson(group))
+      .filter((group) => group.startTime.isAfter(now));
+  });
 
 export const selectGroupError = createSelector(
   [selectStudyGroup],
